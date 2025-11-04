493e96bbd21d8ab72d4c959ae90da263c5d80c72
igorrm19 <igormachado14@gmail.com>
2025-11-04 18:53:09 -0300

    refactor: clean up code structure and remove redundant sections

--- Project_Baldim/src/App/shared/features/login/ui/cardLogin/card.ts
+++ Project_Baldim/src/App/shared/features/login/ui/cardLogin/card.ts
@@ -1,11 +1,12 @@
 
 import template from "./card.html?raw"
-import { ContrutorCardLogin } from "./construtor"
+//import { ContrutorCardLogin } from "./construtor"
 import { Login } from "../login/login"
+import { Main } from "../../../../../../../fox/main"
 
 // responsavel por implementar outros componentes
 
-export class CardLogin extends ContrutorCardLogin {
+export class CardLogin extends Main {
     containerCardLogin: HTMLElement
 
     constructor() {
@@ -17,10 +18,11 @@ export class CardLogin extends ContrutorCardLogin {
         parent.appendChild(this.containerCardLogin)
 
         const login = new Login()
+        
 
         login.addProps({
-            h1_primaryText: "Titulo do formulario",
-            h3_segundoTexto: "Subtitulo",
+            h1_primaryText: "Fox",
+            h3_segundoTexto: "Realize seu login para continuar",
             label_terceiroTexto: "Email",
             label_quartoTexto: "Senha",
         })

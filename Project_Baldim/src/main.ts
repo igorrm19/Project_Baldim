4341fd8ba24294edb62d26e4011bfddce16b59ea
igorrm23 <igormachadofire14@gmail.com>
2025-12-08 19:46:22 -0300

    Testes  via antigravity para ter uma noção de como irei fazer, eesse codigo não repressenta o codigo que sera realizado

--- Project_Baldim/src/main.ts
+++ Project_Baldim/src/main.ts
@@ -2,6 +2,9 @@ import '../src/style.css'
 import { FoxRouter } from '../fox/core/src/module/router/router';
 import { MainPage } from './App/shared/pages/mainPage';
 import { AboutPage } from './App/shared/pages/aboutPage';
+import { teste } from './converto.stringforobject.teste';
+
+teste()
 
 const routes = {
   "/": MainPage,

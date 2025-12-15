291658b75db5f62a27fab346676d478633bd1c89
igorrm23 <igormachadofire14@gmail.com>
2025-12-15 03:08:27 -0300

    refactor: extract DOM parsing logic from main.ts into a new dom parser module.

--- Project_Baldim/src/main.ts
+++ Project_Baldim/src/main.ts
@@ -4,36 +4,11 @@ import { MainPage } from './App/shared/pages/mainPage';
 import { AboutPage } from './App/shared/pages/aboutPage';
 import { teste } from './converto.stringforobject.teste';
 import { html } from './App/shared/features/login/ui/cardLogin/card';
+import { parseHTML } from '../fox/core/src/module/dom/parser';
 
-export type ActionItem = {
-  div: string | any
-}
 
-function testeTween(html: string): Record<string, any> {
-
-  //Uso do DOMParser para converter a string HTML em um documento HTML
-  const parse = new DOMParser()
-  const doc = parse.parseFromString(html, 'text/html')
-  const divs = doc.querySelectorAll('div')
-
-  //Uso de uma pilha para armazenar as views
-  const pilha: ActionItem[] = []
-  divs.forEach(div => {
-    pilha.push({ div: div })
-  })
-
-  //Retorno do objeto com a view
-  return {
-    "DIV": {
-      "ID": divs,
-      "view": pilha[pilha.length], pilha
-    }
-  }
-}
-
-console.log(testeTween(html))
+console.log(parseHTML(html))
 teste()
-
 
 
 const routes = {
